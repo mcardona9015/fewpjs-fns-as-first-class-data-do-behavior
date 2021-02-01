0fbd75e0de60e68571e32684be5139fb1812dfa9
mcardona9015 <mcardona9015@gmail.com>
2021-01-31 21:30:01 -0500

    commit tree 3794cd4cf701b6f17d2228f34b6b8da175aa02f9

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let splitTime = time.split(":"); 
+  let parsedTime =  parseInt(splitTime[0])
+  if (parsedTime < 12) {
+       return "Good Morning";
+  }
+  if (parsedTime >= 12 && parsedTime < 17) {
+       return "Good Afternoon";
+  }
+  if (parsedTime >= 17) {
+       return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let greeting = document.getElementById('greeting');
+  greeting.innerHTML = string;
+}
